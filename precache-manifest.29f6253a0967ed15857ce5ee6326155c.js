d4003268078e01925a6d4ec6a346b21a05d50aff
1lander <landervanlandeghem@hotmail.com>
2020-10-25 19:55:57 +0100

    Updates

---
+++ precache-manifest.29f6253a0967ed15857ce5ee6326155c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae969b09bf63f44c215ab0cab0407dc5",
+    "url": "/osrshighscore/index.html"
+  },
+  {
+    "revision": "2cbc96e6c227d8ed1240",
+    "url": "/osrshighscore/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "73059888e70c02d23444",
+    "url": "/osrshighscore/static/js/2.6a0f3311.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/osrshighscore/static/js/2.6a0f3311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbc96e6c227d8ed1240",
+    "url": "/osrshighscore/static/js/main.80fc1b96.chunk.js"
+  },
+  {
+    "revision": "c1c8bac2399cadf02b03",
+    "url": "/osrshighscore/static/js/runtime-main.192fea67.js"
+  }
+]);
