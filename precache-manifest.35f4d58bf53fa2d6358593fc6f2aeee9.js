0c9408cf6cc02bde2303014c215fda159458c990
1lander <landervanlandeghem@hotmail.com>
2020-10-25 20:22:57 +0100

    Updates

---
+++ precache-manifest.35f4d58bf53fa2d6358593fc6f2aeee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1f71e80a73527f212e3e1d96d3a1ab",
+    "url": "/osrshighscores/index.html"
+  },
+  {
+    "revision": "867df8db0283789cb4cd",
+    "url": "/osrshighscores/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "73059888e70c02d23444",
+    "url": "/osrshighscores/static/js/2.6a0f3311.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/osrshighscores/static/js/2.6a0f3311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867df8db0283789cb4cd",
+    "url": "/osrshighscores/static/js/main.c51e4454.chunk.js"
+  },
+  {
+    "revision": "9385e4903a8396742776",
+    "url": "/osrshighscores/static/js/runtime-main.b54a2267.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/osrshighscore/precache-manifest.75611c8ff69812f74249f12e0fbb5187.js"
+  "/osrshighscores/precache-manifest.35f4d58bf53fa2d6358593fc6f2aeee9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/osrshighscore/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/osrshighscores/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
